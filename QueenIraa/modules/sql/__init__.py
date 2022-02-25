67e3df46b8aebaaca17c3e7e201a6183278168b3
izzy <hitokizzy@gmail.com>
2022-02-25 21:12:09 +0700

    fix db

--- QueenIraa/modules/sql/__init__.py
+++ QueenIraa/modules/sql/__init__.py
@@ -4,6 +4,9 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 
 from QueenIraa import DB_URI
 
+DB_URI = os.getenv("DATABASE_URL")  # or other relevant config var
+if DB_URI and DB_URI.startswith("postgres://"):
+    DB_URI = DB_URI.replace("postgres://", "postgresql://", 1)
 
 def start() -> scoped_session:
     engine = create_engine(DB_URI, client_encoding="utf8")
